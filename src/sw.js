5ca0e79f25cd657ade1447eb61aeb5cc87a5ff6e
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-13 12:25:40 +0545

    Add network caching

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,8 @@
-/* eslint-disable */
+workbox.routing.registerRoute(
+  new RegExp(`https://mock-io.herokuapp.com/.*`),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'api-cache',
+  }),
+);
 
-workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
