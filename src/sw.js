44cf3befb3dfdc4ef2f9968912b133466b26b3ef
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-13 13:59:02 +0545

    Cache google fonts

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,29 @@
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
 workbox.routing.registerRoute(
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-  new workbox.strategies.StaleWhileRevalidate(),
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
 );
 
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+// Cache API response
 workbox.routing.registerRoute(
   new RegExp(`https://mock-io.herokuapp.com/.*`),
   new workbox.strategies.NetworkFirst({
@@ -10,4 +31,5 @@ workbox.routing.registerRoute(
   }),
 );
 
+// Cache static files
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
