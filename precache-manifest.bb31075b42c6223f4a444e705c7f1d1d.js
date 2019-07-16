e0bd8c242b6e9d4cc2ca3993c40f2af30fddb60e
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-16 21:20:56 +0545

    Updates

---
+++ precache-manifest.bb31075b42c6223f4a444e705c7f1d1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eea719c6ec32a3199a0251494dedd98",
+    "url": "/react-pwa-example/index.html"
+  },
+  {
+    "revision": "4dfc3ea0d597e9a8fa2e",
+    "url": "/react-pwa-example/static/css/main.ca943108.chunk.css"
+  },
+  {
+    "revision": "0b7c7c934f7f2823517b",
+    "url": "/react-pwa-example/static/js/2.93997617.chunk.js"
+  },
+  {
+    "revision": "4dfc3ea0d597e9a8fa2e",
+    "url": "/react-pwa-example/static/js/main.f9cc5863.chunk.js"
+  },
+  {
+    "revision": "adaa4f14feb1b0cc553f",
+    "url": "/react-pwa-example/static/js/runtime~main.af49c7ea.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+importScripts("/react-pwa-example/precache-manifest.bb31075b42c6223f4a444e705c7f1d1d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets'
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30
+      })
+    ]
+  })
+);
+
+// Cache API response
+workbox.routing.registerRoute(
+  new RegExp(`https://mock-io.herokuapp.com/.*`),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'api-cache'
+  })
+);
+
+// Cache static files
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
