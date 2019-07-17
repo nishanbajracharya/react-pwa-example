3a0982177feca3f49b3141ae7becb6af64d830b6
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-17 09:37:25 +0545

    Updates

--- precache-manifest.bdf02cb2f765d803828c7eb2b3828899.js
+++ precache-manifest.bdf02cb2f765d803828c7eb2b3828899.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d595c5ff144fb147694bd9640843d36b",
+    "revision": "4974f5887680138631ac2b2b22cf2ca7",
     "url": "/react-pwa-example/index.html"
   },
   {
-    "revision": "ff5ebbbaf7252b095795",
-    "url": "/react-pwa-example/static/css/main.ca943108.chunk.css"
+    "revision": "e849258ef17847e5de71",
+    "url": "/react-pwa-example/static/css/main.4242c2e5.chunk.css"
   },
   {
     "revision": "0b7c7c934f7f2823517b",
     "url": "/react-pwa-example/static/js/2.93997617.chunk.js"
   },
   {
-    "revision": "ff5ebbbaf7252b095795",
-    "url": "/react-pwa-example/static/js/main.683c4a71.chunk.js"
+    "revision": "e849258ef17847e5de71",
+    "url": "/react-pwa-example/static/js/main.28438e56.chunk.js"
   },
   {
     "revision": "adaa4f14feb1b0cc553f",
