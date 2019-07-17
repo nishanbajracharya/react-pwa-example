ab5c546723d5ca989e5ad177db5701e5804cf8ce
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-17 09:44:06 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -33,6 +33,19 @@ workbox.routing.registerRoute(
   })
 );
 
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
 // Cache static files
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
